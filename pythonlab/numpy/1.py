33b98c3c6a120e7ea1714f63e85ab97e2c0110f3
RiyaShrivastawa <130436769+RiyaShrivastawa@users.noreply.github.com>
2024-08-06 14:52:22 +0530

    Add files via upload

---
+++ pythonlab/numpy/1.py
@@ -0,0 +1,4 @@
+import numpy as np
+my_list = [1, 2, 3, 4, 5]
+array_list = np.array(my_list)
+print("Created array is:",array_list  )
---
+++ pythonlab/numpy/2.py
@@ -0,0 +1,6 @@
+import numpy as np
+my_list = [1, 2, 3, 4, 5]
+array_new = np.array(my_list)
+print("First element is :", array_new[0])
+print("\nlast element is :", array_new[4])
+print("After doubling each element of array :", array_new*2)
