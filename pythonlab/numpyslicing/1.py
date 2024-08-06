a2f542cbee62524cd30ff1466ec965eb79516081
RiyaShrivastawa <130436769+RiyaShrivastawa@users.noreply.github.com>
2024-08-06 14:55:26 +0530

    Add files via upload

---
+++ pythonlab/numpyslicing/1.py
@@ -0,0 +1,10 @@
+import numpy as np
+zeros_array = np.zeros(10)
+ones_array = np.ones(10)
+fives_array = np.full(10, 5)
+print("Zeros array:", zeros_array)
+print("Ones array:", ones_array)
+print("Fives array:", fives_array)
+
+
+
---
+++ pythonlab/numpyslicing/4.py
@@ -0,0 +1,8 @@
+import numpy as np
+my_list = [1, 2, 3, 4, 5, 6, 7, 8]
+my_tuple = ([8, 4, 6], [1, 2, 3])
+array_from_list = np.array(my_list)
+array_from_tuple = np.array(my_tuple)
+
+print("Array from list:", array_from_list)
+print("Array from tuple:", array_from_tuple)
